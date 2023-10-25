3cd70ea0b969163265d7b84d629bd8ab4a38d15c
hieu.pham <hieu.pham@coderpush.com>
2023-10-25 14:37:36 +0700

    add loading component

---
+++ components/Loading/LoadingIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+
+const antIcon = <LoadingOutlined className="text-[#F16521]" style={{ fontSize: '70px' }} spin />;
+const LoadingIcon = () => {
+  return (
+    <div className="relative">
+      <Spin size="large" indicator={antIcon} />
+    </div>
+  );
+};
+
+export default LoadingIcon;
---
+++ components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoadingIcon from '@/components/Loading/LoadingIcon';
+
+export const WrapperLoadingIcon = ({ title }: { title?: string }) => {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 top-0 z-50 grid items-center justify-center bg-black/50">
+      <LoadingIcon />
+    </div>
+  );
+};
